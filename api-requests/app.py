2c1b53a9b6819f7eda56ea532c1ecaa6b53e12b2
antopolskiy <s.antopolsky@gmail.com>
2021-04-11 13:33:33 +0200

    homework1 v3

--- api-requests/app.py
+++ api-requests/app.py
@@ -10,7 +10,7 @@ logger = logging.getLogger("app")
 class ConfigError(Exception):
     pass
 
-def read_config(config_path, app="app"):
+def read_config(config_path, app):
     with open(config_path, "r") as yaml_file:
         config = yaml.safe_load(yaml_file)
     try:
@@ -28,18 +28,21 @@ def get_auth_token(config):
     return auth_token
 
 def get_out_of_stock(config):
-    resp = requests.get(urljoin(config["url"], config["out_of_stock"]["end_point"]),
-                        headers={"Authorization": get_auth_token(config)},
-                        json=config["out_of_stock"]["payload"])
-    resp.raise_for_status()
-    return json.loads(resp.content)
+    data = {}
+    for date in config["out_of_stock"]["dates"]:
+        resp = requests.get(urljoin(config["url"], config["out_of_stock"]["end_point"]),
+                            headers={"Authorization": get_auth_token(config)},
+                            json={"date": date})
+        resp.raise_for_status()
+        data[date] = json.loads(resp.content) 
+    return data
 
 def make_data_dir(date):
     data_dir = os.path.join("data", date)
     try:
         os.makedirs(data_dir, exist_ok=False)
     except OSError:
-        logger.warning(f"{data_dir} already exists")
+        logger.warning(f"{data_dir} already exists, overriding.")
     return data_dir
 
 def _set_logger(verbose):
@@ -48,19 +51,22 @@ def _set_logger(verbose):
         logger.setLevel(logging.DEBUG)
     else:
         logger.setLevel(logging.WARNING)
+        
+def store_data_for_the_date(date, out_of_stock):
+    data_dir = make_data_dir(date)
+    json_file_path = os.path.join(data_dir, "out_of_stock.json")
+    with open(json_file_path, "w") as json_file:
+        json.dump(out_of_stock, json_file)
+    logger.info(f"Successfully stored response to {json_file_path}")
 
-def store_out_of_stock(config_path, app="app", verbose=False):
+def store_out_of_stock(config_path="config.yml", app="app", verbose=False):
     _set_logger(verbose)
 
     config = read_config(config_path, app=app)
-    resp = get_out_of_stock(config)
-    data_dir = make_data_dir(config['out_of_stock']['payload']['date'])
-
-    with open(os.path.join(data_dir, "out_of_stock.json"), "w") as json_file:
-        json.dump(resp, json_file)
+    out_of_stock_data = get_out_of_stock(config)
+    for date, out_of_stock in out_of_stock_data.items():
+        store_data_for_the_date(date, out_of_stock)
         
 if __name__ == "__main__":
     import fire
     fire.Fire(store_out_of_stock)
-
-# store_out_of_stock("config.yml", verbose=1)
--- api-requests/readme.md
+++ api-requests/readme.md
@@ -1,7 +1,20 @@
 Store response data from `out_of_stock` end point of a target url.
 
-Usage:
+Basic usage:
 
 ```bash
-python app.py config.yml --verbose
+python app.py
+```
+
+Parameters:
+    
+```
+--config_path
+  Path to the config file, default: "./config.yml"
+
+--app
+  With app configuration from the config file to use, default: "app"
+        
+--verbose
+  Show debug logging.
 ```
